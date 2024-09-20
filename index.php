e1a712b1e4397e15c0aaee664446fe7884ecf667
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-20 18:11:37 +0400

    hi

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
 
     <div class="h-16"></div>
-    <div class="bg-red-700 flex items-center justify-center" style="background-color: rgb(112, 8, 8)">
+    <div class=" flex items-center justify-center" style="background-color: #f0f9fa;">
       <div class="bg-white rounded-lg max-w-7xl p-6 md:p-12 shadow-lg -mt-12 mb-20">
         <h1 class="text-center text-3xl font-bold mb-12 text-gray-800">
           Setintel - Where Sports And Innovation Play Together
