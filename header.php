c60c574f40388bbb8a583a68523d5715efa0d584
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-20 16:30:15 +0400

    hi

--- header.php
+++ header.php
@@ -78,24 +78,21 @@ $p = @$_GET['p'];
             <a href="index.php" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
           </div>
 
-            <?php
-            if($p == "s") {
-            ?>
+            <?php if($p == "s") { ?>
             <div class="flex justify-center items-center relative h-9 px-6 cursor-pointer rounded-[20px] border border-[#910101]">
-
-              <?php
-              } else {
-              ?>
+              <?php } else { ?>
                 <div class="flex justify-center items-center relative h-9 px-6  rounded-[20px] border border-[#fff] hover:rounded-[20px] hover:border hover:border-[#910101]">
-                <?php
-               }
-              ?>
-            <a href="services.php" class="text-sm font-semibold leading-6 text-gray-900">Services</a>
+              <?php } ?>
+            <a href="services.php?p=s" class="text-sm font-semibold leading-6 text-gray-900">Services</a>
           </div>
 
 
-          <div class="flex justify-center items-center relative h-9 px-6 rounded-[20px] border border-[#fff] hover:rounded-[20px] hover:border hover:border-[#910101]">
-            <a href="whoweare.php" class="text-sm font-semibold leading-6 text-gray-900">Who we are</a>
+                <?php if($p == "w") { ?>
+                <div class="flex justify-center items-center relative h-9 px-6 cursor-pointer rounded-[20px] border border-[#910101]">
+                    <?php } else { ?>
+                    <div class="flex justify-center items-center relative h-9 px-6  rounded-[20px] border border-[#fff] hover:rounded-[20px] hover:border hover:border-[#910101]">
+                        <?php } ?>
+                        <a href="whoweare.php?p=w" class="text-sm font-semibold leading-6 text-gray-900">Who we are</a>
           </div>
           <div class="flex justify-center items-center relative h-9 px-6  rounded-[20px] border border-[#fff] hover:rounded-[20px] hover:border hover:border-[#910101]">
             <a href="partners.php" class="text-sm font-semibold leading-6 text-gray-900">Partners</a>
