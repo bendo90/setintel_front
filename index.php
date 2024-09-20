19c2f907ce9b15ffcff7007a96a9680dca05c2c4
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-20 16:48:07 +0400

    hi

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
           <div class="md:w-1/3 flex justify-center mt-6 md:mt-0">
             <img
               class="rounded-lg shadow-lg w-full max-w-xs"
-              src="https://via.placeholder.com/300x400"
+              src="images/9.jpg"
               alt="Working on laptop"
             />
           </div>
