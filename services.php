fa74538bfc6dd02e9e89475d91f001ba2eef3c57
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-22 01:48:57 +0400

    hi

--- services.php
+++ services.php
@@ -17,7 +17,7 @@
 
 
 
-  <div class="grid grid-cols-3 gap-4 mx-auto max-w-7xl p-3.5 lg:px-8">
+  <div class="grid grid-cols-3 gap-10 mx-auto max-w-7xl mb-12 lg:px-8">
 
 
       <div style="width: 100%; margin: 0 auto" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow h-100">
