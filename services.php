7ec3b33ad07ba6fc9f56003f7991363b95d7a8c2
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-22 01:32:00 +0400

    hi

--- services.php
+++ services.php
@@ -17,13 +17,13 @@
 
 
 
-  <div class="grid grid-cols-3 gap-4">
+  <div class="grid grid-cols-3 gap-4 max-w-7xl">
       <div>01</div>
-      <div>01</div>
-      <div>01</div>
-      <div>01</div>
-      <div>01</div>
-      <div>09</div>
+      <div>02</div>
+      <div>03</div>
+      <div>04</div>
+      <div>05</div>
+      <div>06</div>
   </div>
 
 
