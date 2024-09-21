3498be6fb250a8b24e15594fc26613b561fc28c6
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-22 02:16:21 +0400

    hi

--- services.php
+++ services.php
@@ -36,22 +36,8 @@
                   that drive growth and engagement.
               </p>
               <div class="pb-5 pt-5">
-                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-teal-400 rounded-lg focus:ring-4 focus:outline-none ">
+                  <a href="#" class="inline-flex items-center py-2 text-sm font-medium text-center text-teal-400 rounded-lg focus:ring-4 focus:outline-none ">
                       Read more
-                      <svg
-                              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 14 10">
-                          <path
-                                  stroke="currentColor"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                      </svg>
                   </a>
               </div>
           </div>
@@ -74,23 +60,8 @@
                   that maximize your brand’s visibility and impact
               </p>
               <div class="pb-5 pt-5">
-                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-teal-400 rounded-lg focus:ring-4 focus:outline-none">
+                  <a href="#" class="inline-flex items-center py-2 text-sm font-medium text-center text-teal-400 rounded-lg focus:ring-4 focus:outline-none">
                       Read more
-                      <svg
-                              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 14 10"
-                      >
-                          <path
-                                  stroke="currentColor"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                      </svg>
                   </a>
               </div>
           </div>
@@ -113,23 +84,8 @@
                   offers.
               </p>
               <div class="pb-5 pt-10">
-                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-teal-400 rounded-lg focus:ring-4 focus:outline-none ">
+                  <a href="#" class="inline-flex items-center py-2 text-sm font-medium text-center text-teal-400 rounded-lg focus:ring-4 focus:outline-none ">
                       Read more
-                      <svg
-                              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 14 10"
-                      >
-                          <path
-                                  stroke="currentColor"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                      </svg>
                   </a>
               </div>
           </div>
