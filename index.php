5079a89101b112bfc931c9088c8ad053529e2428
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-24 03:15:06 +0400

    hi

--- index.php
+++ index.php
@@ -24,57 +24,57 @@
 
 
 
-        <div class=" flex items-center justify-center" style="background-color: #f0f9fa;">
-            <div class="bg-white rounded-lg max-w-7xl p-6 md:p-12 shadow-lg -mt-12 mb-20">
-                <h1 class="text-center text-3xl font-bold mb-12 text-gray-800">
-                    Setintel - Where Sports And Innovation Play Together
-                </h1>
 
-                <div class="md:flex md:space-x-6">
-                    <div class="md:w-2/4 space-y-4">
-                        <p class="text-gray-700 leading-relaxed">
-                            Welcome to Setintel, where innovation meets sports media.
-                            Leveraging our extensive experience, we excel in facilitating
-                            partnerships with local sports media and broadcasters, offering
-                            cutting-edge inventory solutions tailored to enhance your
-                            business. Our expertise spans the integration of gambling
-                            companies into sports ad platforms, significantly boosting
-                            engagement and driving growth.
-                        </p>
-                        <p class="text-gray-700 leading-relaxed pt-5">
-                            Our journey began with the successful operation of the Adjarsport
-                            OTT platform in Georgia, where our strategic partnership with
-                            AdjaraBet, the market's leading gambling company, delivered
-                            impressive advertising results. Now, with the launch of Setanta's
-                            sports OTT platform, we continue to push boundaries and set new
-                            standards in the industry.
-                        </p>
-                        <p class="text-gray-700 leading-relaxed pt-5">
-                            At Setintel, we are dedicated to helping our partners achieve
-                            remarkable growth. Our innovative strategies have proven to
-                            increase first-time deposits (FTD) by up to 30%, double bet
-                            counts, triple bet amounts, and enhance monthly retention rates by
-                            4%. Join us as we revolutionize the sports media landscape with
-                            unparalleled expertise and visionary solutions.
-                        </p>
-                    </div>
-
-                    <div class="md:w-2/4 flex justify-center mt-6 md:mt-0">
-                        <img
-                                class="rounded-lg shadow-lg w-full"
-                                src="images/9.png"
-                                alt="Working on laptop"
-                        />
-                    </div>
-                </div>
-            </div>
-        </div>
 
 
 
     </div>
 
+      <div class=" flex items-center justify-center" style="background-color: #f0f9fa;">
+          <div class="bg-white rounded-lg max-w-7xl p-6 md:p-12 shadow-lg -mt-12 mb-20">
+              <h1 class="text-center text-3xl font-bold mb-12 text-gray-800">
+                  Setintel - Where Sports And Innovation Play Together
+              </h1>
 
+              <div class="md:flex md:space-x-6">
+                  <div class="md:w-2/4 space-y-4">
+                      <p class="text-gray-700 leading-relaxed">
+                          Welcome to Setintel, where innovation meets sports media.
+                          Leveraging our extensive experience, we excel in facilitating
+                          partnerships with local sports media and broadcasters, offering
+                          cutting-edge inventory solutions tailored to enhance your
+                          business. Our expertise spans the integration of gambling
+                          companies into sports ad platforms, significantly boosting
+                          engagement and driving growth.
+                      </p>
+                      <p class="text-gray-700 leading-relaxed pt-5">
+                          Our journey began with the successful operation of the Adjarsport
+                          OTT platform in Georgia, where our strategic partnership with
+                          AdjaraBet, the market's leading gambling company, delivered
+                          impressive advertising results. Now, with the launch of Setanta's
+                          sports OTT platform, we continue to push boundaries and set new
+                          standards in the industry.
+                      </p>
+                      <p class="text-gray-700 leading-relaxed pt-5">
+                          At Setintel, we are dedicated to helping our partners achieve
+                          remarkable growth. Our innovative strategies have proven to
+                          increase first-time deposits (FTD) by up to 30%, double bet
+                          counts, triple bet amounts, and enhance monthly retention rates by
+                          4%. Join us as we revolutionize the sports media landscape with
+                          unparalleled expertise and visionary solutions.
+                      </p>
+                  </div>
+
+                  <div class="md:w-2/4 flex justify-center mt-6 md:mt-0">
+                      <img
+                              class="rounded-lg shadow-lg w-full"
+                              src="images/9.png"
+                              alt="Working on laptop"
+                      />
+                  </div>
+              </div>
+          </div>
+      </div>
 
   </div>
 
