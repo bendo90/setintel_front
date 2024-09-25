8b0ecd24844d2d27b60a65b9b5518aa99ba90250
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-25 17:44:07 +0400

    hi

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="assets/style.css" />
+    <link rel="icon" type="image/png" href="assets/fav.png"/>
     <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body>
