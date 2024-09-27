903b1f2d811f7afb05fc8c916751fd25b848a721
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-27 15:10:05 +0400

    hi

--- mwapi.php
+++ mwapi.php
@@ -1,7 +1,7 @@
 <?php
 
-$myfile = fopen("txt.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("txt.txt"));
+$myfile = fopen("url.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("url.txt"));
 fclose($myfile);
 
 ?>
