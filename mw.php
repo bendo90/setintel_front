a1de17c18be891a45f379359a327699828fa5bdc
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-09-27 14:54:07 +0400

    hi

--- mw.php
+++ mw.php
@@ -14,7 +14,9 @@
       <input type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
   </div>
   <div class="mb-6">
-      <button type="button" class="mb zm aap aej akh arx asn awt axb bbd bcj bjo bpu bpv bpx bqe"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="hp oj so"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path></svg>Button text</button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+      </button>
   </div>
 
 
