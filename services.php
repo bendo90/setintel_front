6e85a2aca97447b250dccbed9b53ca7db255f29b
vscodeart <160016529+vscodeart@users.noreply.github.com>
2024-10-01 13:37:16 +0400

    hi

--- services.php
+++ services.php
@@ -15,7 +15,17 @@
       <p class="text-red-900 font-bold text-4xl">Services</p>
   </div>
 
-
+  <div class="bg-base-200 collapse">
+      <input type="checkbox" class="peer" />
+      <div
+              class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          Click me to show/hide content
+      </div>
+      <div
+              class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          <p>hello</p>
+      </div>
+  </div>
 
   <div class="grid lg:grid-cols-3 gap-10 mx-auto max-w-7xl mb-24 lg:px-8">
 
